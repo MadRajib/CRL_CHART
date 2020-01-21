dfcace0fcac9e5d628ab4df08502ac04b0771708
MadRajib <madhabsharma94@gmail.com>
2020-01-21 21:14:35 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ function loadJSON(callback) {
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', '../data/G_SAC_DATA.json', true);
+    xobj.open('GET', '/data/G_SAC_DATA.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             callback(xobj.responseText);
