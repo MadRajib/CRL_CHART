0cb02eb702612f12a69a948fe08b5888a3dfb2bd
MadRajib <madhabsharma94@gmail.com>
2020-01-21 19:38:54 +0530

    commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$( "#form1" ).submit(function( event ) {
+    event.preventDefault();
+
+    var gsac = $("#measurement").val();
+    var scale = $("#dropdownMenuButton").html(); 
+    if(scale == "cm"){
+        gsac = parseInt(parseFloat(gsac)*10);
+    }
+    console.log(gsac)
+    loadJSON(function(response) {
+        // Parse JSON string into object
+        var data = JSON.parse(response);
+        
+        table = data.G_SAC_DATA.G_SAC_TABLE_MM;
+        table.forEach(element => {
+            if(element.gsac == gsac){
+                alert("Gestational Age :"+ element.gage);
+                return;
+            }
+        });
+        
+    });
+
+});
+
+$(".dropdown-menu a").click(function(){
+    var selText = $(this).text();
+    
+    $("#dropdownMenuButton").html(selText); 
+  });
+
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '../data/G_SAC_DATA.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);  
+ }
